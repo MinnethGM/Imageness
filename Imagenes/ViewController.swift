a9f2e02e4f9237c30d5da58118a4f0d25e019328
Maestro <Maestro@iMac-de-Maestro-9.local>
2018-08-28 18:45:59 -0700

    uno

--- Imagenes/ViewController.swift
+++ Imagenes/ViewController.swift
@@ -10,9 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgSecuencia: UIImageView!
+    @IBOutlet weak var lblTexto: UILabel!
+    @IBAction func doTapGato(_ sender: Any) {
+        lblTexto.text = "Meow Pariente"
+        if imgSecuencia.isAnimating{
+        imgSecuencia.stopAnimating()
+            lblTexto.text = "Meow Pariente"
+        }else {
+        imgSecuencia.startAnimating()
+            lblTexto.text = "continua!"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imgSecuencia.image = UIImage(named: "Carta2_00000")
+        
+        
+        var imagenes : [UIImage] = []
+        imagenes.append(UIImage(named: "Carta2_00000")!)
+        imagenes.append(UIImage(named: "Carta2_00001")!)
+        imagenes.append(UIImage(named: "Carta2_00003")!)
+        imagenes.append(UIImage(named: "Carta2_00004")!)
+        imagenes.append(UIImage(named: "Carta2_00005")!)
+        imagenes.append(UIImage(named: "Carta2_00006")!)
+        imagenes.append(UIImage(named: "Carta2_00007")!)
+        
+        imgSecuencia.animationImages = imagenes
+        imgSecuencia.animationDuration = 1
+        
+        imgSecuencia.startAnimating()
+    
+        let miGato = gatoo()
+        miGato.nombre = "Don Gato"
+        miGato.vidas = 3
+        
+        
+        // !    es  unwrapping
+        if miGato.nombre != nil{
+        let nombreGato : String = miGato.nombre!
+        
+        lblTexto.text = nombreGato
+        }
+        
+    
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Imagenes/gatoo.swift
@@ -0,0 +1,16 @@
+//
+//  gatoo.swift
+//  Imagenes
+//
+//  Created by Maestro on 28/08/18.
+//  Copyright © 2018 Maestro. All rights reserved.
+//
+
+import Foundation
+
+class gatoo {
+    var nombre : String?
+    var vidas : Int?
+    var edad : Int?
+    var esterilizado : Bool?
+}
